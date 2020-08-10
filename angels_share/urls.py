48b6519c141ddbca4655429ac73895f740f119ec
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-08-10 12:21:38 +0000

    added whiskey clubs views and template

--- angels_share/urls.py
+++ angels_share/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('whiskey_clubs/', include('clubs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import whiskey_club
+
 
 # Create your views here.
+
+
+def whiskey_clubs(request):
+    """ a view to return the different whiskey clubs users can subscribe to"""
+    
+    whiskey_clubs = whiskey_club.objects.all()
+
+    context = {
+        'whiskey_clubs': whiskey_clubs,
+    }
+
+    return render(request, 'clubs/whiskey_clubs.html', context)
