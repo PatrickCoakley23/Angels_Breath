8155e19317443806bb84deda1c522b3d1ad4469b
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-08-12 10:32:38 +0000

    added subscriptions models to database

--- clubs/models.py
+++ clubs/models.py
@@ -2,18 +2,33 @@ from django.db import models
 
 # Create your models here.
 
+
 class Whiskey_club(models.Model):
-    name = models.CharField(max_length=254)					
-    comment = models.CharField(max_length=254)	
+    name = models.CharField(max_length=254)
+    comment = models.CharField(max_length=254)
     description = models.TextField()
     tasting_notes = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2,null=True, blank=True)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)	
-    image_url = models.URLField(max_length=1024, null=True, blank=True)	
+    price = models.DecimalField(max_digits=6, decimal_places=2,
+                                null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2,
+                                 null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-	
 
     def __str__(self):
         return self.name
 
 
+class Subscription_type(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2,
+                                null=True, blank=True)
+    whiskey_clubs = models.ManyToManyField(Whiskey_club,
+                                           through='Subscriptions')
+
+
+class Subscriptions(models.Model):
+    whiskey_club = models.ForeignKey(Whiskey_club, on_delete=models.CASCADE)
+    Subscription_type = models.ForeignKey(Subscription_type,
+                                          on_delete=models.CASCADE)
--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -6,11 +6,16 @@ def shopping_cart(request):
 
     return render(request, 'shopping_cart/cart.html')
 
+
 def add_to_cart(request, club_id):
     """ Add club before deciding subscription"""
     quantity = 1
     cart = request.session.get('cart', {})
 
+    if cart.items():
+        # insert toast here (error)
+        return redirect('clubs')
+    
     cart[club_id] = cart.get(club_id, quantity)
 
     request.session['cart'] = cart
