cb7b518179e150acddec76f62a140a0ed3e28d3a
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-08-14 06:55:11 +0000

    deployment version 2, added heroku app to allowed hosts

--- angels_share/settings.py
+++ angels_share/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('M4SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['angels-share.herokuapp.com', 'localhost']
 
 
 # Application definition
