64d962bc5e0e4642db7d5cf7ba5b99661650d54f
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-08-19 23:16:22 +0000

    about to push to heroku master again just defined local host

--- angels_share/settings.py
+++ angels_share/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5wqblj(io28jfl$fs!%*njabfe^e+=y=-qr0093u8%0z#8_jvu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['angels-share.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'angels_share.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
